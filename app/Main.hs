95adc57dc71f8f9dbdf6657da466f5b5c983f00c
José Manuel Calderón Trilla <jmct@jmct.cc>
2022-06-29 04:08:21 -0400

    Add ability to apply N64 font decoding to dumps

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified Data.Pak as P
 import           Data.Pak.Parse (parsePak)
+import           Data.N64Font (decodeBS)
 import           Data.Pak.DumpNote (dumpNote)
 
 import qualified Data.ByteString.Lazy as BS
@@ -15,22 +16,26 @@ main :: IO ()
 main = do
   opt    <- getOpts
   case opt of
-    Opt mode (FPath f) Stdout -> do
+    Opt mode af (FPath f) Stdout -> do
       let handle = stdout
       processed <- fmap (>>= P.processPak) (parsePak f)
       case processed of
         Left err -> IO.hPutStrLn stderr err
-        Right pp -> doDump handle mode pp
+        Right pp -> doDump af handle mode pp
 
-doDump h Meta         pp = IO.hPutStr h (P.displayPak pp)
-doDump h (DumpPage i) pp = dumpPage h i pp
-doDump h (DumpNote i) pp =
+doDump _  h Meta         pp = IO.hPutStr h (P.displayPak pp)
+doDump af h (DumpPage i) pp = dumpPage af h i pp
+doDump _  h (DumpNote i) pp =
   case dumpNote i pp of
     Left err -> IO.hPutStrLn stderr err
     Right bs -> BS.hPutStr h bs
 
-dumpPage :: IO.Handle -> Int -> P.Pak P.Processed -> IO ()
-dumpPage h i (P.Pak _ _ _ pgs) =
+dumpPage :: Bool -> IO.Handle -> Int -> P.Pak P.Processed -> IO ()
+dumpPage af h i (P.Pak _ _ _ pgs) =
   case P.getPage i pgs of
     Nothing -> IO.hPutStr stderr ("Could not find page # " ++ show i)
-    Just bs -> BS.hPutStr h bs
+    Just bs -> BS.hPutStr h (font bs)
+ where
+  font bs
+    | af        = decodeBS bs
+    | otherwise = id bs
