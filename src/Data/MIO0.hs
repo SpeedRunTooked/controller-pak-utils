d50284c53e65855a5506fd0cd95b26e5f1835f47
José Manuel Calderón Trilla <jmct@jmct.cc>
2022-07-01 03:11:17 -0400

    WIP: Start on decompression algorithm

--- src/Data/MIO0.hs
+++ src/Data/MIO0.hs
@@ -2,7 +2,7 @@ module Data.MIO0 where
 
 import           Data.Word
 import           GHC.Int
-import           Data.Bits (shiftR)
+import           Data.Bits (shiftR, shiftL, (.|.), (.&.))
 
 import           Control.Applicative
 import           Control.Monad.Except
@@ -65,6 +65,7 @@ word8ToBools w = go 8 []
 data MIOState = MIOState { layout_   :: [Bool]
                          , outputB_  :: Vector Word8
                          , index_    :: Int
+                         , len_      :: Int
                          , comped_   :: ByteString
                          , uncomped_ :: ByteString
                          }
@@ -73,30 +74,101 @@ data MIOState = MIOState { layout_   :: [Bool]
 type MIO = ExceptT String (State MIOState)
 
 mkInitMIOState :: ByteString -> MIORawHeader -> MIOState
-mkInitMIOState dat (MIORH _ l c u bs) = MIOState bs vec 0
+mkInitMIOState dat (MIORH _ l c u) = MIOState lbs vec 0 (fromIntegral l) cOff uOff
  where
-  vec  = V.replicate (fromIntegral l) 0
+  vec  = V.replicate (fromIntegral l) 0 :: Vector Word8
   cOff = BS.drop ((fromIntegral c) - 16) dat
   uOff = BS.drop ((fromIntegral u) - 16) dat
+
+  lbs  = getLayout dat
 
 
 outputB :: MIO (Vector Word8)
 outputB = gets outputB_
 
+putByte :: Word8 -> MIO ()
+putByte b =
+ do
+  buf <- outputB
+  i   <- index
+  let nvec = (V.//) buf [(i,b)]
+  modify (\s -> s { outputB_ = nvec })
+
 layout :: MIO [Bool]
 layout = gets layout_
 
--- getByteU :: MIO Word8
--- getByteU =
---  do
-  
+nextLBit :: MIO Bool
+nextLBit =
+ do
+  lbss <- layout
+  let lb:lbs = lbss
+  modify (\s -> s { layout_ = lbs })
+  return lb
 
+index :: MIO Int
+index = gets index_
 
-runMIO :: ByteString -> Either String ByteString
+ipp :: MIO ()
+ipp = modify (\s -> s { index_ = index_ s + 1 })
+
+comped :: MIO ByteString
+comped = gets comped_
+
+uncomped :: MIO ByteString
+uncomped = gets uncomped_
+
+compByte :: MIO Word8
+compByte =
+ do
+  res <- BS.uncons <$> comped
+  case res of
+    Nothing     -> throwError "Couldn't uncons in compByte"
+    Just (b,bs) ->
+     do
+      modify (\s -> s { uncomped_ = bs })
+      return b
+
+unCompByte :: MIO Word8
+unCompByte =
+ do
+  res <- BS.uncons <$> uncomped
+  case res of
+    Nothing     -> throwError "Couldn't uncons in unCompByte"
+    Just (b,bs) ->
+     do
+      modify (\s -> s { uncomped_ = bs })
+      return b
+
+runMIO :: ByteString -> Either String MIOState
 runMIO bs =
  do
   (rest, header) <- getMIOHeader bs
-  flip evalState (mkInitMIOState header) $ (runExceptT (uncompress rest))
+  flip evalState (mkInitMIOState rest header) $ runExceptT (uncompress >> get)
 
-uncompress :: ByteString -> MIO ByteString
-uncompress = undefined
+isEnd :: MIO Bool
+isEnd = (>=) <$> index <*> (fmap V.length) outputB
+
+calcLenOff :: Word8 -> Word8 -> (Word8, Word16)
+calcLenOff b1 b2 = (l, upperOff + lowerOff + 1)
+ where
+  l = ((b1 .&. 0xF0) `shiftR` 4) + 3
+
+  upperOff = (fromIntegral (b1 .&. 0x0F)) `shiftL` 8
+  lowerOff = fromIntegral b2
+
+
+
+readCompressed :: MIO ()
+readCompressed =
+ do (l,o) <- calcLenOff <$> compByte <*> compByte
+    undefined
+
+uncompress :: MIO ()
+uncompress =
+ do b <- nextLBit
+    if b
+     then unCompByte >>= putByte
+     else readCompressed
+    end <- isEnd
+    unless end
+      uncompress
