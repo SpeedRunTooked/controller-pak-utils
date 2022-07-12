ec2bb333b86d1dd6e6d10081ce563086e044cbf6
José Manuel Calderón Trilla <jmct@umd.edu>
2022-07-12 10:57:55 -0400

    Generalize Data.Pak.DumpNote
    Idea here is simple, we may want to process the note in other ways!
    
    We still have `dumpNote`, but we have now added:
    
    * `withNote`
    * `mioOffsets`
    
    Both `mioOffsets` and `dumpNote` are implemented with `withNote`.

--- src/Data/Pak/DumpNote.hs
+++ src/Data/Pak/DumpNote.hs
@@ -1,4 +1,9 @@
-module Data.Pak.DumpNote where
+{-# LANGUAGE OverloadedStrings #-}
+module Data.Pak.DumpNote
+  ( dumpNote
+  , withNote
+  , mioOffsets
+  ) where
 
 import qualified Data.ByteString.Lazy as BS
 import           Data.ByteString.Lazy    (ByteString)
@@ -9,14 +14,30 @@ import Data.Pak.Types
 import Data.Pak.INodeTable
 import Data.Pak.NoteTable
 
+import GHC.Int
+
 dumpNote :: Int -> Pak Processed -> Either String ByteString
-dumpNote i (Pak _ inT (NT es) ps)
-  | i >= length es = Left "Invalid index for Note"
+dumpNote i pak = withNote i pak pure
+
+withNote :: Int -> Pak Processed -> (ByteString -> Either String a) -> Either String a
+withNote i (Pak _ inT (NT es) ps) f
+  | i >= length es = Left "Invalid index for Note: withNote"
   | otherwise      =
     case L.find sameStart (iNodeFiles inT) of
       Nothing               -> Left "Couldn't find Note in INodeTable"
-      Just (FileLoc _ _ is) -> case getPages is ps of
-                                Nothing -> Left "Unable to get all Pages"
-                                Just bs -> Right bs
+      Just (FileLoc _ _ is) ->
+        case getPages is ps of
+          Nothing -> Left "Unable to get all Pages"
+          Just bs -> f bs
+
  where
   sameStart (FileLoc s _ _) = s == (startPage (es !! i))
+
+mioOffsets :: Int -> Pak Processed -> Either String [Int64]
+mioOffsets i pak = withNote i pak search
+
+search :: ByteString -> Either String [Int64]
+search bs = pure [i | (True, i) <- zip mios is]
+ where
+  is   = BS.findIndices (== 77) bs
+  mios = map (BS.isPrefixOf "MIO0" . snd . flip BS.splitAt bs) is
